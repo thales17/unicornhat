6d1d646aa0e0831746880107072ca1081d902b3f
Adam Richardson <ajrich01@gmail.com>
2017-08-20 16:37:18 -0400

    Added more colors

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Unicronhat
-This is a set of python scripts that are meant to be run on a raspberry pi with a pimoroni unicornhat
+This is a set of python scripts that are meant to be run on a raspberry pi with a pimoroni unicornhat.
--- patterns.py
+++ patterns.py
@@ -12,12 +12,25 @@ width, height = unicorn.get_shape()
 
 colors = {
     'red': {'r': 255, 'g': 0, 'b': 0},
-    'green': {'r': 0, 'g': 255, 'b': 0}
+    'green': {'r': 0, 'g': 255, 'b': 0},
+    'blue': {'r': 0, 'g': 0, 'b': 255},
+    'yellow': {'r': 255, 'g': 255, 'b': 0},
+    'orange': {'r': 255, 'g': 128, 'b': 0},
+    'purple': {'r': 255, 'g': 0, 'b': 255},
+    'cyan': {'r': 0, 'g': 255, 'b': 255},
+    'gray': {'r': 128, 'g': 128, 'b': 128},
 }
 
 pixels = [
     {'x': 0, 'y': 0,  'color': 'red'},
     {'x': 1, 'y': 0,  'color': 'green'},
+    {'x': 2, 'y': 0,  'color': 'blue'},
+    {'x': 3, 'y': 0,  'color': 'yellow'},
+    {'x': 4, 'y': 0,  'color': 'orange'},
+    {'x': 5, 'y': 0,  'color': 'purple'},
+    {'x': 6, 'y': 0,  'color': 'cyan'},
+    {'x': 7, 'y': 0,  'color': 'gray'},
+    
 ]
 
 for pixel in pixels:
